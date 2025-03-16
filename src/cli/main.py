6b3ea81d11f5f53943bb021622d445ce15e60ca3
guylain237 <guylainboss2@gmail.com>
2025-03-16 17:56:15 +0100

    amelioration de mon architecture

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,24 +1,14 @@
 # cli/main.py
-
-import os
-import sys
-import argparse
-
-# Ajouter le répertoire racine du projet dans le PYTHONPATH
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.utils import get_logger
 
 # Importer la fonction de création de la base de données
-from databases.createdb import create_db
+from src.databases import create_db
 
-def main(args):
+def main():
     # Création de la base de données à partir du script schema.sql
+    logger = get_logger(__name__)
+    logger.info("Creation de la base de données...")
     create_db()
-
+    logger.info("Base de données créée avec succès !")
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description="Création de la base de données pour le projet Nobels"
-    )
-    parser.add_argument("--verbose", action="store_true", help="Mode verbeux")
-    args = parser.parse_args()
-    
-    main(args)
+    main()
--- src/databases/createdb.py
+++ src/databases/createdb.py
@@ -1,31 +1,31 @@
 import sqlite3
 import os
-
+from src.utils import DB_PATH, SCHEMA_PATH
 def create_db():
-    #chemin absolu
-    base_dir = os.path.dirname(__file__)
+   
 
     #chemin relatif vers la base de données et le schema
-    db_path = os.path.join(base_dir, 'Nobels.db')
-    schema_path = os.path.join(base_dir, 'schema.sql')
+    DB_PATH 
+    SCHEMA_PATH
+    
 
     #connexion à la base de données
-    connexion = sqlite3.connect(db_path)
+    connexion = sqlite3.connect(DB_PATH)
     cursor = connexion.cursor()
 
 
     # Vérifier que le fichier schema.sql existe
-    if not os.path.isfile(schema_path):
-        raise FileNotFoundError(f"Le fichier {schema_path} n'existe pas")
+    if not os.path.isfile(SCHEMA_PATH):
+        raise FileNotFoundError(f"Le fichier {SCHEMA_PATH} n'existe pas")
         return
 
     # Lire et exécuter le script SQL
-    with open(schema_path, 'r', encoding='utf-8') as f:
+    with open(SCHEMA_PATH, 'r', encoding='utf-8') as f:
         schema_script = f.read()
     cursor.executescript(schema_script)
     
     # Sauvegarder les modifications et fermer la connexion
     connexion.commit()
     connexion.close()
-    print(f"Base de données '{db_path}' créée avec succès !")
+    print(f"Base de données '{DB_PATH}' créée avec succès !")
     
